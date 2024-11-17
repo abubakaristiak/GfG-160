c6a9a84963733fb36ffb5aa70d877ef6f27df712
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-11-17 23:06:54 +0600

    added GfG 160 - 160 Days of Problem Solving of Day-02

---
+++ Day -02/Reverse an Array.cpp
@@ -0,0 +1,46 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2024-11-17 || 23:00:15
+ * * * * File    : Reverse an Array.cpp
+ */
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        reverse(arr.begin(), arr.end());
+        
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
